fd17d8b8bd297b4bd1deca167edc15541daa9679
mitchelleokemwa <mitchelleokemwa@gmail.com>
2022-12-08 17:30:00 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+function superbowlWin(){
+     [
+        { year: "2015", result: "W"},
+        { year: "2014", result: "N/A"},
+        { year: "2013", result: "L"},
+        //...
+      ]
+    
+}
+ 
+
+
